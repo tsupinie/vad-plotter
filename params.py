b3b8c54c26c862e1f326321134c90bd342b8c0cd
Tim Supinie <tsupinie@ou.edu>
2015-12-29 01:03:24 -0600

    Rewrote the hodograph plotter, added critical angle calculations.

--- params.py
+++ params.py
@@ -29,8 +29,18 @@ def compute_parameters(data, storm_motion):
     storm_u = -storm_spd * np.sin(storm_dir)
     storm_v = -storm_spd * np.cos(storm_dir)
 
-    u_1km, u_3km, u_6km = np.interp([ 1., 3., 6. ], data['altitude'], u, left=np.nan, right=np.nan)
-    v_1km, v_3km, v_6km = np.interp([ 1., 3., 6. ], data['altitude'], v, left=np.nan, right=np.nan)
+    u_05km, u_1km, u_3km, u_6km = np.interp([ 0.5, 1., 3., 6. ], data['altitude'], u, left=np.nan, right=np.nan)
+    v_05km, v_1km, v_3km, v_6km = np.interp([ 0.5, 1., 3., 6. ], data['altitude'], v, left=np.nan, right=np.nan)
+
+    base_u = storm_u - u[0]
+    base_v = storm_v - v[0]
+    ang_u = u_05km - u[0]
+    ang_v = v_05km - v[0]
+    len_base = np.hypot(base_u, base_v)
+    len_ang = np.hypot(ang_u, ang_v)
+    base_dot_ang = base_u * ang_u + base_v * ang_v
+
+    params['critical'] = np.degrees(np.arccos(base_dot_ang / (len_base * len_ang)))
 
     u_shear_1km, v_shear_1km = compute_shear(u[0], v[0], u_1km, v_1km)
     u_shear_3km, v_shear_3km = compute_shear(u[0], v[0], u_3km, v_3km)
--- plot.py
+++ plot.py
@@ -4,110 +4,127 @@ import numpy as np
 import matplotlib
 matplotlib.use('agg')
 import pylab
-from matplotlib.lines import Line2D
-from matplotlib.transforms import Bbox
-from matplotlib.transforms import TransformedBbox
+#from matplotlib.lines import Line2D
+#from matplotlib.transforms import Bbox
+#from matplotlib.transforms import TransformedBbox
+from matplotlib.patches import Circle
 
+_seg_hghts = [0, 3, 6, 9, 12, 18]
+_seg_colors = ['r', '#00ff00', '#008800', '#993399', 'c']
+
+def _plot_param_table(parameters):
+    trans = pylab.gca().transAxes
+    line_space = 0.028
+    start_x = 1.02
+    start_y = 1.0 - line_space
+
+    line_y = start_y
+
+    kwargs = {'color':'k', 'fontsize':10, 'clip_on':False, 'transform':trans}
+
+    pylab.text(start_x + 0.18, start_y, "Parameter Table", ha='center', fontweight='bold', **kwargs)
+
+    line_y -= line_space
+
+    pylab.text(start_x + 0.095, line_y,          "BWD (kts)", fontweight='bold', **kwargs)
+    pylab.text(start_x + 0.22,  line_y - 0.0011, "SRH (m$^2$s$^{-2}$)", fontweight='bold', **kwargs)
+
+    line_y -= line_space
+
+    pylab.text(start_x, line_y, "0-1 km", fontweight='bold', **kwargs)
+    pylab.text(start_x + 0.095, line_y, "%d" % int(parameters['shear_mag_1km']), **kwargs)
+    pylab.text(start_x + 0.22,  line_y, "%d" % int(parameters['srh_1km']), **kwargs)
+
+    line_y -= line_space
+
+    pylab.text(start_x, line_y, "0-3 km", fontweight='bold', **kwargs)
+    pylab.text(start_x + 0.095, line_y, "%d" % int(parameters['shear_mag_3km']), **kwargs)
+    pylab.text(start_x + 0.22,  line_y, "%d" % int(parameters['srh_3km']), **kwargs)
+
+    line_y -= line_space
+
+    pylab.text(start_x, line_y, "0-6 km", fontweight='bold', **kwargs)
+    pylab.text(start_x + 0.095, line_y, "%d" % int(parameters['shear_mag_6km']), **kwargs)
+
+    line_y -= 1.5 * line_space
+
+    pylab.text(start_x, line_y, "Critical Angle:", fontweight='bold', **kwargs)
+    pylab.text(start_x + 0.18, line_y, "%d$^{\circ}$" % int(parameters['critical']), **kwargs)
+
+def _plot_data(data, storm_motion):
+    storm_dir, storm_spd = storm_motion
+
+    u = -data['wind_spd'] * np.sin(np.radians(data['wind_dir']))
+    v = -data['wind_spd'] * np.cos(np.radians(data['wind_dir']))
+    alt = data['altitude']
+
+    storm_u = -storm_spd * np.sin(np.radians(storm_dir))    
+    storm_v = -storm_spd * np.cos(np.radians(storm_dir))    
+
+    seg_idxs = np.searchsorted(alt, _seg_hghts)
+    seg_u = np.interp(_seg_hghts, alt, u, left=np.nan, right=np.nan)
+    seg_v = np.interp(_seg_hghts, alt, v, left=np.nan, right=np.nan)
+    ca_u = np.interp([0.5], alt, u, left=np.nan, right=np.nan)
+    ca_v = np.interp([0.5], alt, v, left=np.nan, right=np.nan)
+
+    for idx in xrange(len(_seg_hghts) - 1):
+        idx_start = seg_idxs[idx]
+        idx_end = seg_idxs[idx + 1]
+
+        if not np.isnan(seg_u[idx]):
+            pylab.plot([seg_u[idx], u[idx_start]], [seg_v[idx], v[idx_start]], '-', color=_seg_colors[idx], linewidth=1.5)
+
+        pylab.plot(u[idx_start:idx_end], v[idx_start:idx_end], '-', color=_seg_colors[idx], linewidth=1.5)
+
+        if not np.isnan(seg_u[idx + 1]):
+            pylab.plot([u[idx_end - 1], seg_u[idx + 1]], [v[idx_end - 1], seg_v[idx + 1]], '-', color=_seg_colors[idx], linewidth=1.5)
+
+        for upt, vpt, rms in zip(u, v, data['rms_error'])[idx_start:idx_end]:
+            rad = 2 * np.sqrt(np.pi) * rms
+            circ = Circle((upt, vpt), rad, color=_seg_colors[idx], alpha=0.1)
+            pylab.gca().add_patch(circ)
+
+    pylab.plot([storm_u, u[0], ca_u], [storm_v, v[0], ca_v], 'c-', linewidth=0.75)
+
+    pylab.plot(storm_u, storm_v, 'k+', markersize=6)
+    pylab.text(storm_u + 0.5, storm_v - 0.5, "SM: %03d/%02d kts" % (storm_dir, storm_spd), ha='left', va='top', color='k', fontsize=10)
+
+
+def _plot_background(max_ring):
+    pylab.axvline(x=0, linestyle='-', color='#999999')
+    pylab.axhline(y=0, linestyle='-', color='#999999')
+
+    for irng in xrange(10, max_ring, 10):
+        ring = Circle((0., 0.), irng, linestyle='dashed', fc='none', ec='#999999')
+        pylab.gca().add_patch(ring)
+
+        pylab.text(irng + 0.5, -0.5, "%d" % irng, ha='left', va='top', fontsize=9, color='#999999', clip_on=True, clip_box=pylab.gca().get_clip_box())
 
 def plot_hodograph(data, parameters, storm_motion):
     img_title = "%s VWP valid %s" % (data.rid, data['time'].strftime("%d %b %Y %H%M UTC"))
     img_file_name = "%s_vad.png" % data.rid
+    max_u = 80
 
-    param_names = {
-        'shear_mag_1km':"0-1 km Bulk Shear",
-        'shear_mag_3km':"0-3 km Bulk Shear",
-        'shear_mag_6km':"0-6 km Bulk Shear",
-        'srh_1km':"0-1 km Storm-Relative Helicity",
-        'srh_3km':"0-3 km Storm-Relative Helicity",
-    }
+    pylab.figure(figsize=(10, 7.5), dpi=150)
+    fig_wid, fig_hght = pylab.gcf().get_size_inches()
+    fig_aspect = fig_wid / fig_hght
 
-    param_units = {
-        'shear_mag_1km':"kt",
-        'shear_mag_3km':"kt",
-        'shear_mag_6km':"kt",
-        'srh_1km':"m$^2$ s$^{-2}$",
-        'srh_3km':"m$^2$ s$^{-2}$",
-    }
+    axes_left = 0.05
+    axes_bot = 0.05
+    axes_hght = 0.9
+    axes_wid = axes_hght / fig_aspect
+    pylab.axes((axes_left, axes_bot, axes_wid, axes_hght))
 
-    pylab.clf()
-    pylab.axes((0.05, 0.05, 0.65, 0.85), polar=True)
+    _plot_background(int(np.ceil(max_u * np.sqrt(2))))
+    _plot_data(data, storm_motion)
+    _plot_param_table(parameters)
 
-    rms_error_colors = ['c', 'g', 'y', 'r', 'm']
-    rms_error_thresh = [0, 2, 6, 10, 14, 999]
+    pylab.xlim(-max_u / 2, max_u)
+    pylab.ylim(-max_u / 2, max_u)
+    pylab.xticks([])
+    pylab.yticks([])
 
-    wind_dir = np.radians(data['wind_dir'])
-
-    u = -data['wind_spd'] * np.sin(wind_dir)
-    v = -data['wind_spd'] * np.cos(wind_dir)
-
-    u_marker = np.interp(np.arange(16, dtype=float), data['altitude'], u, left=np.nan, right=np.nan)
-    v_marker = np.interp(np.arange(16, dtype=float), data['altitude'], v, left=np.nan, right=np.nan)
-    ws_marker = np.hypot(u_marker, v_marker)
-    wd_marker = np.arctan2(-u_marker, -v_marker)
-
-    u_avg = (u[1:] + u[:-1]) / 2
-    v_avg = (v[1:] + v[:-1]) / 2
-
-    wind_spd_avg = np.hypot(u_avg, v_avg)
-    wind_dir_avg = np.arctan2(-u_avg, -v_avg)
-
-    color_index = np.where((data['rms_error'][0] >= rms_error_thresh[:-1]) & (data['rms_error'][0] < rms_error_thresh[1:]))[0][0]
-    pylab.polar([ wind_dir[0], wind_dir_avg[0] ], [ data['wind_spd'][0], wind_spd_avg[0] ], color=rms_error_colors[color_index], lw=1.5)
-
-    for idx in range(1, len(data['wind_dir']) - 1):
-        color_index = np.where((data['rms_error'][idx] >= rms_error_thresh[:-1]) & (data['rms_error'][idx] < rms_error_thresh[1:]))[0][0]
-        pylab.polar([ wind_dir_avg[idx - 1], wind_dir[idx], wind_dir_avg[idx] ], [ wind_spd_avg[idx - 1], data['wind_spd'][idx], wind_spd_avg[idx] ], color=rms_error_colors[color_index], lw=1.5)
-
-    color_index = np.where((data['rms_error'][-1] >= rms_error_thresh[:-1]) & (data['rms_error'][-1] < rms_error_thresh[1:]))[0][0]
-    pylab.polar([ wind_dir_avg[-1], wind_dir[-1] ], [ wind_spd_avg[-1], data['wind_spd'][-1] ], color=rms_error_colors[color_index], lw=1.5)
-
-    u_500_marker = np.interp(0.5, data['altitude'], u, left=np.nan, right=np.nan)
-    v_500_marker = np.interp(0.5, data['altitude'], v, left=np.nan, right=np.nan)
-
-    ws_500_marker = np.hypot(u_500_marker, v_500_marker)
-    wd_500_marker = np.arctan2(-u_500_marker, -v_500_marker)
-
-    pylab.polar(wd_500_marker, ws_500_marker, ls='None', mec='k', mfc='k', marker='o', ms=3)
-    pylab.polar(wd_marker, ws_marker, ls='None', mec='k', mfc='k', marker='o', ms=4)
-
-    storm_direction, storm_speed = storm_motion
-    pylab.plot(np.pi * storm_direction / 180., storm_speed, 'k+')
-
-    pylab.gca().set_theta_direction(-1)
-    pylab.gca().set_theta_zero_location('S')
-    pylab.gca().set_thetagrids(np.arange(0, 360, 30), labels=np.arange(0, 360, 30), frac=1.05)
-    pylab.gca().set_rgrids(np.arange(10, 80, 10), labels=np.arange(10, 70, 10), angle=15)
-    pylab.ylim((0, 70))
-
-    pylab.text(wind_dir[0], data['wind_spd'][0], "%3.1f" % data['altitude'][0], size='small', weight='bold')
-    pylab.text(wind_dir[-1], data['wind_spd'][-1], "%3.1f" % data['altitude'][-1], size='small', weight='bold')
-
-    start_x = 1.07
-    start_y = 0.62
-
-    for idx, param_key in enumerate(sorted(parameters.keys())):
-        if np.isnan(parameters[param_key]):
-            param_text = "%s\n  %f" % (param_names[param_key], parameters[param_key])
-        else:
-            param_text = "%s\n  %.1f %s" % (param_names[param_key], parameters[param_key], param_units[param_key])
-
-        pylab.text(start_x, start_y - idx * 0.075, param_text, transform=pylab.gca().transAxes, size='x-small', weight='bold')
-
-    start_y = 0.25
-
-    pylab.text(start_x, start_y, "RMS Errors:", transform=pylab.gca().transAxes, size='x-small', weight='bold')
-
-    for idx, color in enumerate(rms_error_colors):
-        line = Line2D([start_x, start_x + 0.05], [start_y + 0.012 - (idx + 1) * 0.0375, start_y + 0.012 - (idx + 1) * 0.0375], color=color, clip_on=False, lw=1.5, transform=pylab.gca().transAxes)
-        pylab.gca().add_line(line)
-        if rms_error_thresh[idx + 1] == 999:
-            legend_text = "%d kt+" % rms_error_thresh[idx]
-        else:
-            legend_text = "%d-%d kt" % (rms_error_thresh[idx], rms_error_thresh[idx + 1])
-        pylab.text(start_x + 0.075, start_y - (idx + 1) * 0.0375, legend_text, transform=pylab.gca().transAxes, size='x-small', weight='bold')
-
-    pylab.suptitle(img_title)
+    pylab.title(img_title)
     pylab.savefig(img_file_name)
-    return
-
+    pylab.close()
 
