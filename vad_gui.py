7e499fd7f79716b296ac806587f396a3d3c7c6d0
Tim Supinie <tsupinie@ou.edu>
2016-03-13 22:33:10 -0500

    Added margins to hodograph

--- vad_gui.py
+++ vad_gui.py
@@ -13,18 +13,20 @@ class VADWidget(QtGui.QWidget):
         self._min_v, self._max_v = -40., 80.
         self._drng = 10
 
-        self._pix_wid, self._pix_hgt = 400, 400
+        self._pix_wid, self._pix_hgt = 500, 500
+        self._mrg_wid, self._mrg_hgt = 10, 10
+        self._hodo_rect = QtCore.QRect(self._mrg_wid, self._mrg_hgt, self._pix_wid - 2 * self._mrg_wid, self._pix_hgt - 2 * self._mrg_hgt)
 
         self._initWidget()
 
     def _pix2uv(self, pix_x, pix_y):
-        u_pt = self._min_u + (self._max_u - self._min_u) * float(pix_x) / self._pix_wid
-        v_pt = self._max_v - (self._max_v - self._min_v) * float(pix_y) / self._pix_hgt
+        u_pt = self._min_u + (self._max_u - self._min_u) * float(pix_x - self._mrg_wid) / (self._pix_wid - 2 * self._mrg_wid)
+        v_pt = self._max_v - (self._max_v - self._min_v) * float(pix_y - self._mrg_hgt) / (self._pix_hgt - 2 * self._mrg_hgt)
         return u_pt, v_pt
 
     def _uv2Pix(self, u_pt, v_pt):
-        pix_x = (u_pt - self._min_u) / (self._max_u - self._min_u) * self._pix_wid
-        pix_y = (self._max_v - v_pt) / (self._max_v - self._min_v) * self._pix_hgt
+        pix_x = (u_pt - self._min_u) / (self._max_u - self._min_u) * (self._pix_wid - 2 * self._mrg_wid) + self._mrg_wid
+        pix_y = (self._max_v - v_pt) / (self._max_v - self._min_v) * (self._pix_hgt - 2 * self._mrg_hgt) + self._mrg_hgt
         return int(round(pix_x)), int(round(pix_y))
 
     def _initWidget(self):
@@ -43,11 +45,13 @@ class VADWidget(QtGui.QWidget):
         pen = QtGui.QPen()
         pen.setStyle(QtCore.Qt.DashLine)
         pen.setWidth(1.5)
-        pen.setBrush(QtGui.QColor('#888888'))
+        pen.setColor(QtGui.QColor('#888888'))
 
         qp = QtGui.QPainter()
         qp.begin(self._background)
         qp.setRenderHint(QtGui.QPainter.Antialiasing, on=True)
+        qp.setClipRect(self._hodo_rect)
+        qp.setClipping(True)
         qp.setPen(pen)
 
         for irng in xrange(self._drng, max_rng, self._drng):
@@ -69,6 +73,12 @@ class VADWidget(QtGui.QWidget):
         x1, y1 = self._uv2Pix(0, self._min_v)
         x2, y2 = self._uv2Pix(0, self._max_v)
         qp.drawLine(x1, y1, x2, y2)
+        qp.setClipping(False)
+
+        pen.setColor(QtCore.Qt.black)
+        pen.setWidth(2)
+        qp.setPen(pen)
+        qp.drawRect(self._hodo_rect)
 
         qp.end()
 
